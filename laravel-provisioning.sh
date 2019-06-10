1b1f54fb7942ebecd0e7e0dea44fe9f13b80b971
Abdulrahman <saanina@gmail.com>
2019-06-11 01:40:00 +0300

    fixes

--- laravel-provisioning.sh
+++ laravel-provisioning.sh
@@ -127,18 +127,30 @@ echo 'server {
     # Make site accessible from http://localhost/
     server_name localhost;
 
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Content-Type-Options "nosniff";
+
+    charset utf-8;
+
     location / {
         try_files $uri $uri/ /index.php?$query_string;
     }
 
-    # Pass all .php files onto a php-fpm/php-fcgi server.
-    location ~ [^/]\.php(/|$) {
-        fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-        if (!-f $document_root$fastcgi_script_name) {
-            return 404;
-        }
-        include fastcgi.conf;
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
+    location ~ \.php$ {
         fastcgi_pass unix:/var/run/php/php7.3-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.(?!well-known).* {
+        deny all;
     }
 }' > /etc/nginx/sites-available/default
 
