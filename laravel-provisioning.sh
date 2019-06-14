244bc46047d33bc9012ae7957600dd9d13f97ebc
Abdulrahman <saanina@gmail.com>
2019-06-14 14:28:20 +0300

    better laravel support

--- laravel-provisioning.sh
+++ laravel-provisioning.sh
@@ -2,8 +2,41 @@
 
 # modified from Laravel 5 homestead provisioning script @ https://github.com/laravel/settler
 # modified from Laravel 5 provisioning script @ https://github.com/mrlami/provisioning-scripts
+# Abdulrahman, 2019-6, github:abdumu, twitter:aphpdev
 
-#please: change secret to anything else for mysql
+#please: change these variables before using it
+
+#mysql root password, and the extra user and database for your app
+MYSQL_ROOT_PASSWORD=""
+MYSQL_DATABASE_USER="" #to use it in your app
+MYSQL_DATABASE_PASSWORD=''
+MYSQL_DATABASE_NAME=""
+
+APP_FOLDER_NAME="test" #your app folder name
+SERVER_NAME="localhost" #change it to your domain or ip
+
+#an extra account that has limited ability, www-data group to modifiy apps folder
+DEPLOYER_USERNAME=""
+DEPLOYER_PASSWORD=""
+#used to access deployer account, i.e. from gitlab ci or docker images
+DEPLOYER_PUBLIC_AUTHORIZED_KEY=""
+
+#if private make sure to include the token
+#github: https://<token>@github.com/owner/repo.git (see https://github.blog/2012-09-21-easier-builds-and-deployments-using-git-over-https-and-oauth/)
+#gitlab: https://<token_name>:<token_secret>:@gitlab.com/owner/repo.git (see https://docs.gitlab.com/ee/user/project/deploy_tokens/)
+LARAVEL_PROJECT_GIT_REPO=""
+LARAVEL_ENV_FILE_TO_PRODUCTION=".env.example" #the name of .env file that we will rename to .env.production
+
+PHP_MEMORY_LIMIT="128M"
+PHP_DATE_TIMEZONE="UTC"
+
+
+
+
+#
+# dont change after this line, or do if you want.
+#
+
 
 # Update Package List
 apt-get update
@@ -41,15 +74,12 @@ php7.3-bz2 php7.3-intl php7.3-json php7.3-readline php7.3-zip
 
 # php7.0-mcrypt is available, but is already compiled in via ppa:ondrej/php
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
 
 # Set Some PHP CLI Settings
 # sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/cli/php.ini
 # sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 128M/" /etc/php/7.3/cli/php.ini
-sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.3/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = $PHP_DATE_TIMEZONE/" /etc/php/7.3/cli/php.ini
 
 # Install Nginx
 apt-get install  -y --force-yes nginx
@@ -62,15 +92,15 @@ service nginx restart
 # sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.3/fpm/php.ini
 # sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.3/fpm/php.ini
 sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.3/fpm/php.ini
-sed -i "s/memory_limit = .*/memory_limit = 128M/" /etc/php/7.3/fpm/php.ini
-sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.3/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = $PHP_MEMORY_LIMIT/" /etc/php/7.3/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = $PHP_DATE_TIMEZONE/" /etc/php/7.3/fpm/php.ini
 
 # Misc Nginx & PHP-FPM Config
 sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
 sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.3/fpm/pool.d/www.conf
 
 service nginx restart
-service php5-fpm restart
+service php7.3-fpm restart
 
 # Install Node
 apt-get install -y nodejs
@@ -79,40 +109,62 @@ apt-get install -y nodejs
 apt-get install -y sqlite3 libsqlite3-dev
 
 # Install MySQL
-debconf-set-selections <<< "mysql-server mysql-server/root_password secret"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again secret"
+debconf-set-selections <<< "mysql-server mysql-server/root_password $MYSQL_ROOT_PASSWORD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again $MYSQL_ROOT_PASSWORD"
 apt-get install -y mysql-server
 
 # Configure MySQL Remote Access
 sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0/' /etc/mysql/my.cnf
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'mysql_root_password' WITH GRANT OPTION;"
 service mysql restart
 
-mysql --user="root" --password="secret" -e "CREATE USER 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret';"
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'homestead'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
-mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'homestead'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
-mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
-mysql --user="root" --password="secret" -e "CREATE DATABASE homestead;"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "CREATE USER '$MYSQL_DATABASE_USER'@'0.0.0.0' IDENTIFIED BY '$MYSQL_DATABASE_PASSWORD';"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_DATABASE_USER'@'0.0.0.0' IDENTIFIED BY '$MYSQL_DATABASE_PASSWORD' WITH GRANT OPTION;"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "GRANT ALL ON *.* TO '$MYSQL_DATABASE_USER'@'%' IDENTIFIED BY '$MYSQL_DATABASE_PASSWORD' WITH GRANT OPTION;"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE $MYSQL_DATABASE_NAME;"
 service mysql restart
 
 # Add Timezone Support To MySQL
-mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=secret mysql
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql --user=root --password=$MYSQL_ROOT_PASSWORD
 
 
 # Install A Few Other Things
-apt-get install -y redis-server
+apt-get install -y redis-server zip unzip acl composer
 
 # Enable Swap Memory
 /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
 /sbin/mkswap /var/swap.1
 /sbin/swapon /var/swap.1
 
-echo 'export $HOME/.composer/vendor/bin:$PATH' >> ~/.bash_profile
-
 mkdir /apps
-mkdir /apps/test
-mkdir /apps/test/public
-echo '<?php echo "hello brother!";' > /apps/test/public/index.php
+chown -R www-data:www-data /apps
+chmod -R 775 /apps
+
+#clone repo
+if [! -z "$LARAVEL_PROJECT_GIT_REPO"]
+    cd /apps
+    git clone "$LARAVEL_PROJECT_GIT_REPO" "$APP_FOLDER_NAME"
+    cd "$APP_FOLDER_NAME"
+    cp "$LARAVEL_ENV_FILE_TO_PRODUCTION" .env
+    sed -i "s/^\(APP_BDOMAIN=\).*/\1$SERVER_NAME/" .env
+    sed -i "s/^\(APP_URL=\).*/\1http\:\/\/$SERVER_NAME/" .env
+    sed -i "s/^\(SESSION_DOMAIN=\).*/\1\.$SERVER_NAME/" .env
+    sed -i "s/^\(DB_DATABASE=\).*/\1$MYSQL_DATABASE_name/" .env
+    sed -i "s/^\(DB_USERNAME=\).*/\1$MYSQL_DATABASE_USER/" .env
+    sed -i "s/^\(DB_PASSWORD=\).*/\1$MYSQL_DATABASE_PASSWORD/" .env
+
+    composer install
+    npm install
+    php artisan key:generate
+    chmod -R 755 storage
+    chmod -R 755 bootstrap/cache
+
+then 
+    mkdir /apps/"$APP_FOLDER_NAME"
+    mkdir /apps/"$APP_FOLDER_NAME"/public
+    echo '<?php echo "hello brother!";' > /apps/"$APP_FOLDER_NAME"/public/index.php
+fi
 
 #for ipv6 add this change
 # listen [::]:80 default_server ipv6only=on;
@@ -121,11 +173,11 @@ echo 'server {
     listen 80 default_server;
     listen [::]:80 default_server;
 
-    root /apps/test/public;
+    root /apps/'"$APP_FOLDER_NAME"'/public;
     index index.html index.htm index.php;
 
     # Make site accessible from http://localhost/
-    server_name localhost;
+    server_name '"$SERVER_NAME"';
 
     add_header X-Frame-Options "SAMEORIGIN";
     add_header X-XSS-Protection "1; mode=block";
@@ -155,10 +207,16 @@ echo 'server {
 }' > /etc/nginx/sites-available/default
 
 
-chown -R www-data:www-data /apps
-chmod -R 775 /apps
 
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
 
+#add new user [deployer for example, and add it to www-data group], to modifiy /apps folder
+adduser "$DEPLOYER_USERNAME" --gecos '' --disabled-password -d
+echo "$DEPLOYER_USERNAME:$DEPLOYER_PASSWORD" | sudo chpasswd
+
+#add ssh public key to access new user for deployment matter
+mkdir -p /home/"$DEPLOYER_USERNAME"/.ssh
+chmod 700 /home/"$DEPLOYER_USERNAME"/.ssh
+echo "$DEPLOYER_PUBLIC_AUTHORIZED_KEY" >> /home/"$DEPLOYER_USERNAME"/.ssh/authorized_keys
